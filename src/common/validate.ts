63a0d48cb5984ded336de74af936d634a62996a8
Oleksey <28filosof28@gmail.com>
2021-04-12 15:34:43 +0300

    feat: added styles and extract validation for auth form

--- src/common/validate.ts
+++ src/common/validate.ts
@@ -1,15 +1,28 @@
 import * as yup from 'yup';
 
-export const signUpSchema = yup.object().shape({
+export const signUpSchema = yup.object({
   name: yup
     .string()
-    .min(2, 'Too short!')
+    .min(3, 'Too short!')
     .max(50, 'Too long!')
-    .required('Required'),
-  email: yup.string().email('Invalid email').required('Required'),
+    .matches(/^([a-zA-Z]\s*)+/i, 'Name is incorrect!')
+    .required('Required!'),
+  email: yup
+    .string()
+    .max(50, 'Too long!')
+    .email('Invalid email')
+    .required('Required!'),
   password: yup
     .string()
     .min(2, 'Too short!')
     .max(50, 'Too long!')
-    .required('Required'),
+    .matches(
+      /(?=.*[0-9])(?=.*[!@#$%^&_*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&_*]{6,}/g,
+      'The password must contain numbers, special characters, lowercase and uppercase Latin letters!'
+    )
+    .required('Required!'),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref('password')], 'Password mismatch!')
+    .required('Required!'),
 });
